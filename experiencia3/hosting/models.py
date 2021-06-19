a1d253f3df82e6b975c94625b3b0927be807c186
stayhc <1mmolationstayhc@gmail.com>
2021-06-19 02:40:15 -0400

    Modificación models

--- experiencia3/hosting/models.py
+++ experiencia3/hosting/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Categoria(models.Model):
 
-    idCategoria = models.IntegerField(primary_key=True, verbose_name='Id de Categoria')
+    idCategoria = models.IntegerField(primary_key=True, verbose_name='Id de Categoría')
     nombreCategoria = models.CharField(max_length=50, verbose_name='Nombre Categoría')
 
     def __str__(self):
@@ -14,8 +14,9 @@ class Categoria(models.Model):
 class Cliente(models.Model):
 
     rut= models.CharField(max_length=9, primary_key=True, verbose_name='Rut')
-    nombre= models.CharField(max_length=30, verbose_name='Nombre del Cliente')
-    tcredito = models.CharField(max_length=20, verbose_name='Numero de tarjeta de credito')
+    nombre= models.CharField(max_length=20, verbose_name='Nombre del Cliente')
+    apellido= models.CharField(max_length=20, verbose_name='Apellido')
+    email = models.EmailField(verbose_name='Correo electrónico')
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
 
     def __str__(self):
